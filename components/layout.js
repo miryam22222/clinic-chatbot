acfccd5d22ac4d35ae07fa1f26a5b89009421545
Arthur Oxenhorn <arthuro@cs.huji.ac.il>
2022-11-20 00:55:22 +0200

    sorting

--- components/layout.js
+++ components/layout.js
@@ -1,25 +1,18 @@
+import { Children } from "react";
 import SortFilter from "./sort-filter";
-
-function getDataQueryKeys(event) {
-    if (event.target.id === "data-sort") {
-        setSortKey(event.target.value);
-    }
-}
 
 // This is our base page layout - See it is used in pages_app.js
 export default function Layout({ children }) {
     return (
-        <body>
-            <div className="row">
-                <div id="container">
-                    <div id="main">
-                        <title>Weather in random locations</title>
-                        <h1 className="content">Weather in the World</h1>
-                        <SortFilter changeHandler={getDataQueryKeys} />
-                        {children}
-                    </div>
+        <div className="row">
+            <div id="container">
+                <div id="main">
+                    <title>Weather in random locations</title>
+                    <h1 className="content">Weather in the World</h1>
+                    <SortFilter />
+                    {children}
                 </div>
             </div>
-        </body>
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@
 import Message from "../components/message";
 import { loadWeatherData } from "../hooks/data";
 import citiesData from '../cities.json';
+import { sortByCities } from '../utils';
+import { useState } from "react";
+
 
 function getEmoji(temperature) {
     if (temperature > 20) {
@@ -13,19 +16,22 @@ function getEmoji(temperature) {
     }
 }
 
+
 // Our main page. Here we are loading data "on the client"
 // And showing some loading screen(s) while waiting for the data to be ready
 export default function IndexPage() {
-
     const { data, isLoading, isError } = loadWeatherData(citiesData);
+    const [sortKey, setSortKey] = useState("-");
     if (isLoading) return <Message content="Loading..." />
     if (isError) return <Message content="An error occured..." />
     if (!data) return <Message content="No data could be loaded..." />
 
+    const cities = sortByCities(data, sortKey);
+
     return (
         <div className="row">
             {
-                data.map(record => {
+                cities.map(record => {
                     const temperature = record.json.current_weather.temperature;
                     return (
                         <div key={record.cityData.name} className="item">
