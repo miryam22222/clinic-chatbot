1d8dd47c8396c7fa210c06f7630a9a5eadf2d6cc
Arthur Oxenhorn <arthuro@cs.huji.ac.il>
2022-11-22 02:19:15 +0200

    finalized logic

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,8 @@ import Message from "../components/message";
 import { loadWeatherData } from "../hooks/data";
 import citiesData from '../cities.json';
 import { sortByCities, filterOut } from '../utils';
-import { createContext, useState } from "react";
+import { useState } from "react";
 import SortFilter from "../components/sort-filter";
-
-const CONTINENTS = 4;
 
 function getEmoji(temperature) {
     if (temperature > 20) {
@@ -19,36 +17,70 @@ function getEmoji(temperature) {
 }
 
 
+const checkboxes = [
+    {
+        name: "America",
+        key: "America",
+        label: "America"
+    },
+    {
+        name: "Europe",
+        key: "Europe",
+        label: "Europe"
+    },
+    {
+        name: "Asia",
+        key: "Asia",
+        label: "Asia"
+    },
+    {
+        name: "Africa",
+        key: "Africa",
+        label: "Africa"
+    }
+];
+
 // Our main page. Here we are loading data "on the client"
 // And showing some loading screen(s) while waiting for the data to be ready
 export default function IndexPage() {
     const { data, isLoading, isError } = loadWeatherData(citiesData);
     const [sortKey, setSortKey] = useState("-");
-    const [checked, setChecked] = useState(new Array(CONTINENTS).fill(true));
+    const [checkedItems, setCheckedItems] = useState("-");
 
     const handleSort = (event) => setSortKey(event.target.value);
-    const handleFilter = (event) => setFilterKey(event.target.value);
+    const handleChecked = (event) => {
+        setCheckedItems({
+            ...checkedItems,
+            [event.target.name]: event.target.checked
+        });
+    }
+
 
     if (isLoading) return <Message content="Loading..." />
     if (isError) return <Message content="An error occured..." />
     if (!data) return <Message content="No data could be loaded..." />
 
-    const cities = filterOut(sortByCities(data, sortKey), filterKey);
+    const cities = filterOut(sortByCities(data, sortKey), checkedItems);
 
     return (
         <>
-            <SortFilter sortHandler={handleSort} filterHandler={handleFilter} />
+            <SortFilter sortHandler={handleSort}
+                checkboxes={checkboxes}
+                checkedItems={checkedItems}
+                handleChange={handleChecked} />
             <div className="row">
                 {
                     cities.map(record => {
-                        const temperature = record.json.current_weather.temperature;
-                        return (
-                            <div key={record.cityData.name} className="item">
-                                <h3 className="city">{record.cityData.name}</h3>
-                                <p className="emoji">{getEmoji(temperature)}</p>
-                                <h1 className="temperature">{temperature}</h1>
-                            </div>
-                        )
+                        if (!record.filtered) {
+                            const temperature = record.json.current_weather.temperature;
+                            return (
+                                <div key={record.cityData.name} className="item">
+                                    <h3 className="city">{record.cityData.name}</h3>
+                                    <p className="emoji">{getEmoji(temperature)}</p>
+                                    <h1 className="temperature">{temperature}</h1>
+                                </div>
+                            )
+                        }
                     })
                 }
             </div>
