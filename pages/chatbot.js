4e4d5e1f39972fe1e627a6836bebdac624cc55b4
Arthur Oxenhorn <oxenarthur@gmail.com>
2022-12-28 18:53:34 +0200

    trying to add chatbot

---
+++ pages/chatbot.js
@@ -0,0 +1,18 @@
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
+
+import config from '../bot/config.js';
+import MessageParser from '../bot/Messageparser.js';
+import ActionProvider from '../bot/ActionProvider.js';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
+    </div>
+  );
+};
--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import { CONTACT_FORM_ENDPOINT } from "../config";
 
