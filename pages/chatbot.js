b22ed2093226a6a42fea8f56da54852e70346aff
Arthur Oxenhorn <oxenarthur@gmail.com>
2022-12-28 19:05:04 +0200

    wip

--- pages/chatbot.js
+++ pages/chatbot.js
@@ -2,7 +2,7 @@ import Chatbot from 'react-chatbot-kit';
 import 'react-chatbot-kit/build/main.css';
 
 import config from '../bot/config.js';
-import MessageParser from '../bot/Messageparser.js';
+import MessageParser from '../bot/MessageParser.js';
 import ActionProvider from '../bot/ActionProvider.js';
 
 const MyComponent = () => {
