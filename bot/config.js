10c7021c1f4e5f2003b682a40daac48a07da47e7
Arthur Oxenhorn <arthuro@cs.huji.ac.il>
2023-01-03 12:50:38 +0200

    dog

--- bot/config.js
+++ bot/config.js
@@ -1,13 +1,14 @@
-import { createChatBotMessage, createCustomMessage } from 'react-chatbot-kit';
+import { createChatBotMessage } from 'react-chatbot-kit';
+import DogPicture from '../components/dogPicture.js';
 
 const config = {
-    initialMessages: [createCustomMessage('value to input', 'custom')],
+    initialMessages: [createChatBotMessage(`Hi! I'm clinicbot`)],
     widgets: [
         {
             widgetName: 'dogPicture',
             widgetFunc: (props) => <DogPicture {...props} />,
         },
-    ]
+    ],
 };
 
 export default config;
--- components/button.js
+++ components/button.js
@@ -3,6 +3,7 @@
 export default function Button({ children, onClick }) {
     return (
         <button onClick={onClick}>
+
             {children}
         </button>
     )
